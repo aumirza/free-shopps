c2c6129f80331c0ec00989a27e52c2dfc765c657
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-04-26 14:06:28 +0530

    feat: integrate Redux store and create apiSlice for data fetching

--- src/providers.tsx
+++ src/providers.tsx
@@ -1,11 +1,15 @@
 import { PropsWithChildren } from "react";
 import { BrowserRouter } from "react-router";
+import { Provider } from "react-redux";
 import { SidebarProvider } from "./components/ui/sidebar";
+import { store } from "./store";
 
 export function Providers({ children }: PropsWithChildren) {
   return (
     <BrowserRouter>
-      <SidebarProvider>{children}</SidebarProvider>
+      <Provider store={store}>
+        <SidebarProvider>{children}</SidebarProvider>
+      </Provider>
     </BrowserRouter>
   );
 }
---
+++ src/services/apiSlice.ts
@@ -0,0 +1,8 @@
+import { API_BASE_URL } from "@/constants";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: API_BASE_URL }),
+  endpoints: () => ({}),
+});
